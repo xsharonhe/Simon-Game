98bfaf88269cb3fb2dff49172c7f8eac45691c44
xsharonhe <xsharonhe@gmail.com>
2020-06-14 19:46:46 -0400

    Change audio files and add initialization of game

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-$("h1")
+var buttonColours = ["blue", "purple", "green", "cream"];
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNum = Math.floor(Math.random()*4);
+
+  var randomColour = buttonColours[randomNum];
+
+  gamePattern.push(randomColour);
+
+  $("#" + randomColour).fadeIn(100),fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(Out);
+}
